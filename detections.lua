ee17b3ff0cac6868c6348a403cf2f8c8de7429da
EggToaster <toshikawa907@gmail.com>
2025-07-18 08:31:59 +0900

    Implemented all features in V2 format

--- detections.lua
+++ detections.lua
@@ -15,14 +15,103 @@ local function _kitchensinked(tocheck)
 end
 
 local trigger = false -- Reserved by two checks
+local bootarg = plist.NVRAM.Add["7C"]["boot-args"]
+local verbosed = bootarg:match("-v$") or bootarg:find("-v ")
 
 local order = {"Info", "Oddities", "Issues", "Kitchen sinked", "Autotool/prebuilt/configurator"}
 local d = {
 	Info = {
-
+		function() -- Tiny bit messy, but it *works*
+			return "SecureBootModel is "..plist.Misc.Security.SecureBootModel.."\n"..
+				"bootarg is "..bootarg.."\n"..
+				"SMBIOS is "..plist.PlatformInfo.Generic.SystemProductName, false
+		end,	
+		function()
+			for _, v in pairs(plist.Kernel.Patch) do
+				if v.Comment:match("AuthenticAMD") then
+					return "This is an AMD machine"
+				end
+			end
+		end,
+		function()
+			if bootarg:find("nvme=[-]1") then -- heckin regex
+				return "NVME is disabled"
+			end
+		end,
+		function()   -- Super awful, but this means igfx or rad
+			if bootarg:find("-[ri][ag][df]x?vesa") or bootarg:find("nv_disable=1") or bootarg:find("-amd_no_dgpu_accel") then
+				return "One or more VESA arg is enabled"                              -- include macOS official arg for AMD
+			end      -- Just works. One function call.
+		end,
+		function()
+			if drivers["OpenCanopy"] then
+				return "OpenCanopy is present (should be post-install stuff)"
+			end
+		end,
+		function()
+			if plist.Kernel.Quirks.AppleXcpmCfgLock or plist.Kernel.Quirks.AppleCpuPmCfgLock then
+				return "CFGLock is enabled"
+			end
+		end,
+		function()
+			if plist.Kernel.Quirks.XhciPortLimit then
+				return "XhciPortLimit is enabled(Catalina or older?)"
+			end
+		end
 	},
 	Oddities = {
-
+		function()
+			if tools.CleanNvram then
+				return "CleanNvram is being used"
+			end
+		end,
+		function()
+			if drivers.OpenHfsPlus then
+				return "OpenHfsPlus is being used"
+			end
+		end,
+		function()
+			local badkexts = {"USBPorts", "UTBDefault", "USBInjectAll", "(IO80211[a-zA-Z0-9]+) "}
+			local matches = {}
+			for _, v in pairs(badkexts) do
+				local match = table.concat(kextsarray, " "):match(v)
+				if match then
+					table.insert(matches, match)
+				end
+			end
+			if matches[1] then
+				return "Detected "..table.concat(matches, ", ")
+			end
+		end,
+		function()
+			if plist.NVRAM.Add["7C"]["csr-active-config"] == "030A0000" then
+				return "SIP is disabled" -- Nobody would disable the entire SIP over filesystem and kext signing, so...
+			end
+		end,
+		function()
+			if #plist.Misc.Tools > 10 then
+				return "More than 10 tools detected"
+			end
+		end,
+		function()
+			if bootarg:find("-no_compat_check") then
+				return "SMBIOS compatibility check is disabled"
+			end
+		end,
+		function()
+			if verbosed then
+				if not bootarg:find("debug=0x") and not plist.Kernel.Quirks.LapicKernelPanic then
+					return "Verbose is enabled, but debug=0x argument nor LapicKernelPanic is present"
+				end
+			end
+		end,
+		function()
+			if verbosed then
+				if not bootarg:find("keepsyms=1") then
+					return "Verbose is enabled, but keepsyms is absent"
+				end
+			end
+		end
 	},
 	Issues = {
 		function()
@@ -34,33 +123,30 @@ local d = {
 				end
 			end
 		end,
-		--[[local audiopath = plist.DeviceProperties.Add["PciRoot(0x0)/Pci(0x1b,0x0)"] or {}
-ck()if audiopath["AAPL,ig-platform-id"] or audiopath["AAPL,snb-platform-id"] or audiopath["framebuffer-patch-enable"] then
-	check "iGPU properties are injected to audio device"
-end
-
-local igpupath = plist.DeviceProperties.Add["PciRoot(0x0)/Pci(0x2,0x0)"] or {}
-ck()if #igpupath > 15 then
-	check "iGPU properties are bloated"
-end
-
-ck()if kexts.Lilu.Comment == "Patch engine" and kexts.Lilu.MinKernel == "8.0.0" then
-	check "OC Clean Snapshot or configurator equivalent is not done"
-end
-
-ck()
-
-ck()
-
-ck()]]
-		function(plist)
+		function()
+			if kexts.Lilu.Comment == "Patch engine" and kexts.Lilu.MinKernel == "8.0.0" then
+				return "OC Clean Snapshot or configurator equivalent is not done"
+			end
+		end,
+		function()
+			if #(plist.DeviceProperties.Add["PciRoot(0x0)/Pci(0x2,0x0)"] or {}) > 15 then
+				return "iGPU properties are bloated"
+			end
+		end,
+		function()
+			local audiopath = plist.DeviceProperties.Add["PciRoot(0x0)/Pci(0x1b,0x0)"] or {}
+			if audiopath["AAPL,ig-platform-id"] or audiopath["AAPL,snb-platform-id"] or audiopath["framebuffer-patch-enable"] then
+				return "iGPU properties are injected to audio device"
+			end
+		end,
+		function()
 			for k in pairs(plist.DeviceProperties) do
 				if k ~= "Add" and k ~= "Delete" and k:sub(1,1) ~= "#" then
 					return (k.." is found in DeviceProperties, outside of Add/Delete")
 				end
 			end
 		end,
-		function(plist)
+		function()
 			if #plist.UEFI.Drivers > 40 then
 				return "More than 40 drivers detected"
 			end
@@ -70,7 +156,7 @@ ck()]]
 				return "More than 2 HFS+ drivers exists"
 			end
 		end,
-		function(plist)
+		function()
 			local platforminfo = plist.PlatformInfo.Generic
 			if platforminfo.SystemProductName == "iMac19,1" and platforminfo.SystemSerialNumber:sub(1, 4) == "W000" then
 				return "PlatformInfo is not set up"
@@ -119,14 +205,14 @@ ck()]]
 	},
 	["Kitchen sinked"] = {}, -- reserved
 	["Autotool/prebuilt/configurator"] = {
-		function(plist) -- Thanks CorpNewt! -- DEAD: Thanks shitlify dev!
+		function() -- Thanks CorpNewt! -- DEAD: Thanks shitlify dev!
 			if plist.Misc.Boot.Timeout == 10 and plist.Misc.Debug.Target == 0 and plist.Misc.Boot.PickerMode == "External" 
-				and plist.NVRAM.Add["7C436110-AB2A-4BBB-A880-FE41995C9F82"]["prev-lang:kbd"] == "en:252" then
+				and plist.NVRAM.Add["7C"]["prev-lang:kbd"] == "en:252" then
 				return "Failed specific autotool detection (V1, if you believe this config.plist is generated with latest tool, make an issue)"
 			end
 			return false -- This should not even trigger nowadays
 		end,
-		function(plist)
+		function()
 			if type(plist.UEFI.Drivers[1]) == "string" then
 				return "OpenCore is outdated(old Drivers schema)"
 			end
@@ -151,14 +237,14 @@ ck()]]
 			end
 			return false
 		end,
-		function(plist)
+		function()
 			if plist.ACPI.Quirks.RebaseRegions then         -- https://www.insanelymac.com/forum/topic/352881-when-is-rebaseregions-necessary/#findComment-2790821
 				return "The user is a porno pro", false     -- Don't need this quirk = Porno Amateur
 			end 									        -- Need this quirk = Porno Pro 
 			return false
 		end,
 		function()
-			if table.concat(drivers, ""):match("AptioFix2Drv") then
+			if table.concat(drivers, ""):find("AptioFix2Drv") then
 				return "AptioFix detected", false -- What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the r/hackintosh Academy, and I've been involved in numerous secret raids on Tonymacx86, and I have over 300 confirmed roasts. I am trained in OsxAptioFix2Drv-free2000 warfare and I'm the top Hackintosher in the entire InsanelyMac armed forces. You are nothing to me but just another UniBeast user. I will wipe you the fuck out with precision the likes of which has never been seen before on this server, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of slavs across the former Soviet Union and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can delete you in over seven hundred ways, and that's just with my Snow Leopard install. Not only am I extensively trained in unarmed macOS installs, but I have access to the entire arsenal of the Acidanthera repo and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clover isn’t that bad” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.
 			end
 			return false
