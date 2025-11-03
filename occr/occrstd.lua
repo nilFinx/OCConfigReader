0165ce533d54bb79e70f5400dbbda97e3811d569
EggToaster <toshikawa907@gmail.com>
2025-11-03 23:28:09 +0900

    Add more stds

--- occr/occrstd.lua
+++ occr/occrstd.lua
@@ -1,8 +1,32 @@
-function asrt(condition, msg)
+function assert(condition, msg)
     if not condition then
         print(msg)
         os.exit(1)
     end
+end
+
+function error(message)
+	print(message)
+	os.exit(1)
+end
+
+-- Spit a warning without causing issues
+function spit(msg)
+	errormsges = errormsges..msg.."\n"
+end
+
+-- Assert spit
+function aspit(condition, msg)
+	if not condition then
+		errormsges = errormsges..msg.."\n"
+	end
+	return condition
+end
+
+-- Spit that thang and return an empty table
+function nulltable(msg)
+	errormsges = errormsges..msg.."\n"
+	return {}
 end
 
 function osxname(v)
