082c04e75519c2dca234b160943433f67506287f
nilfinx <nulflox@disroot.org>
2026-01-30 13:12:34 +0900

    Fix/refactor/update

--- src/acpi_patch.lua
+++ src/acpi_patch.lua
@@ -1,12 +1,15 @@
 
-local injection = function(detections, order, plist)
+local injection = function(args)
+    local detections = args.detections
+    local plist = args.plist
 	detections["ACPI > Patch"] = {}
-	table.insert(order, 2, "ACPI > Patch") -- After "Info"
+    local dects = detections["ACPI > Patch"]
+	table.insert(args.order, 2, "ACPI > Patch") -- After "Info"
     for _, v in pairs(plist.ACPI.Patch) do
         if not v.Enabled then
-            table.insert(detections["ACPI > Patch"], function() end) -- Add count, do nothing
+            table.insert(dects, function() end) -- Add count, do nothing
         else
-            table.insert(detections["ACPI > Patch"], function()
+            table.insert(dects, function()
                 return v.Comment
             end)
         end
--- src/exampleplugin.lua
+++ src/exampleplugin.lua
@@ -1,21 +1,23 @@
--- plist returns the plist in Lua Tables, raw is just the plist as string
--- kexts is Kernel.Add(with key being the kext name), and same goes for tools, drivers, ssdts
--- kextsshow is only reserved for detections.lua, it is rarely used
--- kextsarrar, driversarray is kexts and drivers, in array format to not make table.concat mad
 -- The tool assumes `proppy.lua` or `plugin.lua` is the plugin name by default
 
--- note: NVRAM.Add["7C"] is equal to longer version of that
+-- note: NVRAM.Add[args.util.SevenC] is equal to longer version of that
 
-local injection = function(detections, order, plist, raw, kexts, tools, drivers, ssdts, kextsshow, kextsarray, driversarray)
+local injection = function(args)
+	local detections = args.detections
+	local plist = args.plist
+	local kexts = args.kexts
+	local tools = args.tools
+	local drivers = args.drivers
+	local ssdts = args.ssdts
 	detections["Test!"] = {} -- Creates new category
-	table.insert(order, "Test!") -- append test to the last line
+	table.insert(args.order, "Test!") -- append test to the last line
 	table.insert(detections["Test!"], function() -- Basic check to see if RebaseRegions is enabled
 		if plist.ACPI.Quirks.RebaseRegions then
 			return "RebaseRegions is enabled"
 		end
 	end)
 	table.insert(detections["Test!"], function() -- Bad practice, but works as an example.
-		if not plist.ACPI.Quirks.RebaseRegions then   -- If false is being returned, it will not count towards the total
+		if not plist.ACPI.Quirks.RebaseRegions then   -- If false is being returned as the 2nd, it will not count towards the total
 			return "RebaseRegions is disabled", false
 		end
 		return false
